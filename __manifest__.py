597acdd6b2d8a6e9364d51a23a6d8e94f37a0838
Codev022 <117266281+Codev022@users.noreply.github.com>
2022-12-10 12:49:33 +0100

    Version 10_12_2022
    Version 10/12/2022

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "construction_site_management",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'name': "Construction Site Management",
+    'summary': "Construction Site Management",
+    'description': "Description of Construction Site Management",
+    #'author': "My Company",
+    #'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','stock', 'mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        #'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/chantier.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+
+    'application': True ,
+    'installable': True ,
+    'auto_install': False,
+    'sequence': 1,
+    
 }
